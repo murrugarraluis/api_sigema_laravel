004164e6de4d787c8ccdcdaf61c027a54a5de796
Luis Murrugarra <luis@combativa.com>
2024-08-04 13:24:52 -0500

    feat: :sparkles: add schedule task

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\Configuration;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+
+$search = "end_time_turn";
+$times = Configuration::where('name', 'like', '%' . $search . '%')->get();
+$end_time_db_day = $times->where('name', $search . '_day')->first()->value;
+$end_time_db_night = $times->where('name', $search . '_night')->first()->value;
+
+Schedule::command('send:notification')->everyMinute();
+Schedule::command('close:attendance-sheet')->dailyAt($end_time_db_day);
+Schedule::command('close:attendance-sheet')->dailyAt($end_time_db_night);
