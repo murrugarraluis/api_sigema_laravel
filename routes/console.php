f19388f8b6ca52a26ade34d2d93724b44dc6ae2b
Luis Murrugarra <luis@combativa.com>
2024-08-09 16:46:28 -0500

    :sparkles: feat: add update console

--- routes/console.php
+++ routes/console.php
@@ -3,12 +3,13 @@
 use App\Models\Configuration;
 use Illuminate\Support\Facades\Schedule;
 
+if (app()->environment() !== 'testing') {
+    $search = "end_time_turn";
+    $times = Configuration::where('name', 'like', '%' . $search . '%')->get();
+    $end_time_db_day = $times->where('name', $search . '_day')->first()->value;
+    $end_time_db_night = $times->where('name', $search . '_night')->first()->value;
 
-$search = "end_time_turn";
-$times = Configuration::where('name', 'like', '%' . $search . '%')->get();
-$end_time_db_day = $times->where('name', $search . '_day')->first()->value;
-$end_time_db_night = $times->where('name', $search . '_night')->first()->value;
-
-Schedule::command('send:notification')->everyMinute();
-Schedule::command('close:attendance-sheet')->dailyAt($end_time_db_day);
-Schedule::command('close:attendance-sheet')->dailyAt($end_time_db_night);
+    Schedule::command('send:notification')->everyMinute();
+    Schedule::command('close:attendance-sheet')->dailyAt($end_time_db_day);
+    Schedule::command('close:attendance-sheet')->dailyAt($end_time_db_night);
+}
